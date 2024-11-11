d2ef86d1ce19884a863276675e9bbfc0b08864c4
giohk <giovannaoliveir407@gmail.com>
2024-11-11 14:43:00 -0300

    atividades php

--- Atividade 5/index.php
+++ Atividade 5/index.php
@@ -15,18 +15,8 @@
         <button type="submit">Enviar</button>
     </form>
 
-    <?php
-    if (isset($_POST['nome']) && isset($_POST['idade'])) {
-        $nome = $_POST['nome'];
-        $idade = (int)$_POST['idade']; // Certifique-se de que a idade é um número inteiro
-        if ($idade < 18) { // Corrigido para incluir "if"
-            echo "<p>$nome, você tem $idade anos e, pela lei, só é permitido dirigir a partir dos 18. </p>";
-        } 
-            else {
-            echo "<p>$nome, você tem $idade anos e, pela lei, já tem a idade permitidade para dirigir.  </p>";
-        }
-    }
-    ?>
+    
+
     
 </body>
 </html
--- Atividade 8/index.php
+++ Atividade 8/index.php
@@ -3,25 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
     <link rel="stylesheet" href="styles.css">
-    <title>Show</title>
 </head>
 <body>
+    <div class="Tabela">
+    <img src="/img/brunomars.png">
     <form action="">
         <label for="Nome">
             <span>Nome</span>
+        <br>
             <input type="text" id="Nome" name="Nome">
         </label>
+        <br>
         <label for="Idade">
-            <span>email</span>
+            <span>Email</span>
+            <br>
             <input type="Idade" id="Idade" name="Idade">
         </label>
+        <br>
         <label for="tipo de ingresso">
             <span>Tipo de ingresso</span>
-            <input type="tipo de ingresso" id="tipo de ingresso" name="tipo de ingresso">
+            <br>
+            <select name="ingresso">
+                <option value="Valor1">VIP</option>
+                <option value="Valor2">Regular</option>
+                <option value="Valor3">Basico</option>
+            </select>
+        
         </label>
         <input type="submit" value="login">
     </form>
-   </main>
 </body>
 </html>
